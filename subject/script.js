71489a0cddcbb7f10939188eff9d241c4f92f3f0
Raphaelle Legendre <raphaelle.legendre@etu.minesparis.psl.eu>
2023-03-01 16:57:12 +0100

    fin du cours

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,39 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+let nombres = []  
+let operateurs = []
+let nombre_v = 0
+let keys = (document.getElementsByClassName("calculator__keys"))[0]
+let display = document.getElementsByClassName("calculator__display")[0]
+    for (let clickkey of keys.children)
+    {
+        let content = clickkey.textContent
+        clickkey.addEventListener("click",
+        (event)=>
+        {
+            console.log(content)
+
+            if (display.textContent == 0 &&  !event.target.classList.contains("key--operator"))
+            {
+                display.textContent = content
+                nombre_v = content 
+            }
+            if (event.target.classList.contains("key--operator"))
+            {   
+                nombres.push(nombre_v)
+                operateurs.push(content)
+            }
+            if (event.target.classList.contains("key--equals"))
+            {
+                let res = 0
+            }
+
+            else
+            {   
+                nombre_v = nombre_v *10 + content
+                display.textContent += content
+            }
+        })
+    }
+
